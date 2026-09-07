145c8371415eafee687129c5ac5d13febe27f824
igorjosiel <igorjosielvitalino@gmail.com>
2026-09-07 15:01:49 -0300

    feat: realiza requisição de get posts utilizando a fetch API.

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,9 +1,16 @@
-import { AppLayout } from "../../layouts/App"
-import { CardPost } from "../../components/CardPost"
-import { posts } from "./data"
-import styles from './feed.module.css'
+import { useEffect, useState } from "react";
+import { CardPost } from "../../components/CardPost";
+import styles from './feed.module.css';
 
 export const Feed = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/blog-posts")
+        .then(response => response.json())
+        .then(data => setPosts(data));
+    }, []);
+
     return (
         <main className={styles.grid}>
             {posts.map(post => <CardPost key={post.slug} post={post} />)}
